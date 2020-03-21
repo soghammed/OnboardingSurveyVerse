7f0535b76feeaabb3857e9ef6833e4b69fb160ae
sog <devhammed@gmail.com>
2020-03-21 12:14:04 +0000

    files init

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import StartScreen from './components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import StartScreenStyle from '../styles/StartScreen.scss';
+export default class StartScreen extends React.Component {
+	render() {
+		return (
+			<div class="start_screen">
+				StartScreen
+			</div>
+		)
+	}
+}
